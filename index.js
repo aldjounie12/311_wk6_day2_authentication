bbb422a7724663f5167c53bea09a6b593b76fa82
Gina Gabriel <aldjounie12@yahoo.com>
2019-08-19 00:55:25 -0500

    authentication

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const usersRouter = require('./routers/users');
 const authRouter = require('./routers/auth');
 
 const app = express();
-const port = process.env.PORT || 4001;
+const port = process.env.PORT || 3306;
 
 app.use(bodyParser.json())
 app.use('/users', usersRouter)
--- sql/connection.js
+++ sql/connection.js
@@ -6,9 +6,9 @@ class Connection {
       console.log('creating mysql connection...')
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: '35.184.42.200',
+        host: '35.222.251.213',
         user: 'root',
-        password: 'password',
+        password: 'password2019',
         database: 'admin'
       })
 
